948563783527038296bbeca851d4340f4f4d8b11
ChenXiaohui <15606951991@qq.com>
2018-09-14 11:02:45 +0800

    页面

--- base-web-dao/base-web-dao-api/src/main/java/com/base/web/dao/organization/OrganizationMapper.java
+++ base-web-dao/base-web-dao-api/src/main/java/com/base/web/dao/organization/OrganizationMapper.java
@@ -3,6 +3,9 @@ package com.base.web.dao.organization;
 import com.base.web.bean.po.organization.Organization;
 import com.base.web.dao.GenericMapper;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * 组织管理模块数据映射接口
  * Created by FQ
@@ -15,4 +18,10 @@ public interface OrganizationMapper extends GenericMapper<Organization,Long> {
      * @return
      */
     void deleteNode(Long id);
+
+    /**
+     * 查询设备在内的四级树
+     * @return
+     */
+    List<Map> queryTree();
 }
