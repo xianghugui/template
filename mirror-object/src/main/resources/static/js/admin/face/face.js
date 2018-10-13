cbae3c5bbf111b5e6f963833142a6bc6c523bf88
A181A <2251234678@qq.com>
2018-10-13 17:06:33 +0800

    人脸检索--修改

--- mirror-object/src/main/resources/static/js/admin/face/face.js
+++ mirror-object/src/main/resources/static/js/admin/face/face.js
@@ -199,6 +199,7 @@ $(function () {
                         }
                         html += "</div></div>";
                         return html;
+                        
                     }
                 },
             ]
